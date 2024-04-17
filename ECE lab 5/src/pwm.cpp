6819d4a74b3beff900415c98484c0bc86f741f1b
Cole <98437396+BloodyBlade4@users.noreply.github.com>
2024-04-17 15:47:20 -0700

    Update pwm.cpp
    Updated the prescaler to 1, getting rid of the buzzing sound. Added a chirp.

--- ECE lab 5/src/pwm.cpp
+++ ECE lab 5/src/pwm.cpp
@@ -4,19 +4,30 @@
 
 //Initialize timer for PWM
 void initPWMTimer3(){
-    DDRE |= (1 << DDE3); //set header pin 5 to output
+    
+    //Set to fast PWM, 10 bit, with a prescaler of 1. 
     TCCR3A |= (1 << COM3A1) | (1 << WGM31) | (1 << WGM30);
     TCCR3A &= ~(1 << COM3A0);
     TCCR3B |= (1 << WGM32);
     TCCR3B &= ~(1 << WGM33);
-    TCCR3B |= (1 << CS31);
-    TCCR3B &= ~((1 << CS32) | (1 << CS30));
+    TCCR3B |= (1 << CS30);
+    TCCR3B &= ~((1 << CS32) | (1 << CS31));
+
+    //Set duty cycles
     OCR3A = 0;
-  
+
+    DDRE |= (1 << DDE3); //set header pin 5 to output
 }
 
-//Set volume level, 0 for no sound
+//Set volume level, 0 for no sound. If there is sound, chirp as requested.
 void setVolume(int result)
 {
-    OCR3A = result;
+    if (result == 0)
+        OCR3A = result; 
+    else {
+        OCR3A = result*10;
+        delayMs(100);
+        OCR3A = 0;
+        delayMs(100);
+    }
 }
